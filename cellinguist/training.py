b6b41944b3d3c518fa2c94aacc2888ba794d8eb6
Anthony Cillo <arc85@pitt.edu>
2025-05-28 12:48:01 -0400

    update training script

--- cellinguist/training.py
+++ cellinguist/training.py
@@ -16,7 +16,7 @@ import anndata as ad
 import torch.distributed as dist
 from cellinguist.models.loss import mse_loss_for_expression, compute_similarity_loss
 from cellinguist.data.data_funcs import SingleCellDatasetUnified, collate_fn_unified
-from cellinguist.models.base_model import TokenEmbeddingLayer, FlashTransformerEncoderLayer, MaskedGeneExpressionPredictionHead, WholeGenomeExpressionPredictionHead, DomainClassifier, FullModel, get_random_mask_positions, train_epoch_ddp
+from cellinguist.models.base_model import TokenEmbeddingLayer, FlashTransformerEncoderLayer, MaskedGeneExpressionPredictionHead, MaskedGeneIDPredictionHead, WholeGenomeExpressionPredictionHead, DomainClassifier, FullModel, get_random_mask_positions, train_epoch_ddp
 
 def main():
 
@@ -52,14 +52,16 @@ def main():
     num_of_genes = len(gene_ids)
 
     ## Set domains for normalization (optional)
-    seq_batch_ids = torch.tensor(dat.obs[args.domain_for_grad_rev].factorize()[0])
+    if args.domain_for_grad_rev is not None:
+        seq_batch_ids = torch.tensor(dat.obs[args.domain_for_grad_rev].factorize()[0])
+    else: 
+        seq_batch_ids = None
 
     ## Set conditions (optional)
     if args.condition_data is not None:
         condition_labels = torch.tensor(dat.obs[args.condition_data].factorize()[0])
     else:
         condition_labels = None
-    print(condition_labels)
 
     ## Set other input parameters
     CLS_TOKEN_ID = args.reserved_cls_token
@@ -122,6 +124,11 @@ def main():
         expression_vocab_size=expression_vocab_size
     ).to(device)
 
+    masked_gene_head = MaskedGeneIDPredictionHead(
+        d_model=args.prediction_domain_head_dim,
+        gene_vocab_size=gene_vocab_size
+    ).to(device)
+
     whole_genome_head = WholeGenomeExpressionPredictionHead(
         d_model=args.prediction_domain_head_dim,
         total_gene_count=num_of_genes,
@@ -133,14 +140,26 @@ def main():
 
     # Combine components into a full model.
     # Assume FullModel is a module that takes the token embedding layer, encoder layers, and prediction heads.
-    full_model = FullModel(token_embedding_layer, flash_encoder_layers, masked_head, whole_genome_head, domain_classifier, lambda_value = args.grad_rev_lambda).to(device)
+    full_model = FullModel(token_embedding_layer, flash_encoder_layers, masked_head, whole_genome_head, domain_classifier, masked_gene_head, lambda_value = args.grad_rev_lambda).to(device)
     
+    # Freeze expression embeddings for first epoch
+    for p in full_model.token_embedding_layer.expression_embeddings.parameters():
+        p.requires_grad = False
+
+    for p in full_model.whole_genome_head.parameters():
+        p.required_grad = False
+
     # Wrap the model with DistributedDataParallel.
     ddp_model = DDP(full_model, device_ids=[local_rank])
-    
+
     # Create your dataset and distributed sampler.
     # Assume dataset and collate_fn_unified are defined.
-    optimizer = torch.optim.Adam(ddp_model.parameters(), lr=1e-4)
+    optimizer = torch.optim.Adam(
+        filter(lambda p: p.requires_grad, ddp_model.parameters()),
+        lr = 1e-4
+    )
+
+    #optimizer = torch.optim.Adam(ddp_model.parameters(), lr=1e-4)
 
     # Optionally, if you use teacher forcing, define the number of iterations.
     num_iterative_steps = args.num_iterative_steps
@@ -150,11 +169,20 @@ def main():
     
     for epoch in range(NUM_EPOCHS):
         sampler.set_epoch(epoch)  # shuffle dataset for distributed sampler
-        avg_loss = train_epoch_ddp(
+        if epoch == 1:
+            for p in ddp_model.module.token_embedding_layer.expression_embeddings.parameters():
+                p.requires_grad=True
+            for p in ddp_model.module.whole_genome_head.parameters():
+                p.requires_grad=True
+            optimizer = torch.optim.Adam(
+                filter(lambda p: p.requires_grad, ddp_model.parameters()),
+                lr = 1e-4
+            )
+        avg_loss, loss_masked, loss_genome, loss_similarity, loss_domain, loss_gene_id = train_epoch_ddp(
             dataloader, ddp_model, optimizer, device, num_iterative_steps=num_iterative_steps, mask_token_id=MASK_TOKEN_ID, pad_token_id=PAD_TOKEN_ID
         )
         if local_rank == 0:
-            print(f"Epoch {epoch+1}/{NUM_EPOCHS}, Average Loss: {avg_loss:.4f}")
+            print(f"Epoch {epoch+1}/{NUM_EPOCHS}, Average Loss: {avg_loss:.4f}, Expression Loss: {loss_masked:.4f}, Whole Genome Loss: {loss_genome:.4f}, Similarity Loss: {loss_similarity:4f}, Domain loss: {loss_domain:.4f}, Gene id loss: {loss_gene_id:.4f}")
     
     # Clean up.
     dist.destroy_process_group()
